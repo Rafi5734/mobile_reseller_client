923a13b8dc918b1dc56892df8ddd216d9fe579d2
Rafi5734 <kamrulhasun5734@gmail.com>
2023-08-17 14:37:44 +0600

    search feature added in featured phone section

--- src/pages/home/ad/ProductAd.jsx
+++ src/pages/home/ad/ProductAd.jsx
@@ -1,24 +1,132 @@
-import { Box, Container, SimpleGrid, Stack, Title } from "@mantine/core";
+import { Box, Container, SimpleGrid, Stack, Title, TextInput, ActionIcon, useMantineTheme } from "@mantine/core";
 import useGetProducts from "../../../hooks/phones/useGetProducts";
 import PhoneCard from "../../products/mobile-phones/phone-card/PhoneCard";
+import { IconSearch, IconArrowRight, IconArrowLeft } from "@tabler/icons-react";
+import { useEffect, useState } from "react";
 
 export default function ProductAd() {
+  const theme = useMantineTheme();
   const { products } = useGetProducts({ ad: true });
-  console.log(products);
+  const [searchValue, setSearchValue] = useState();
+  const [allProducts, setAllProducts] = useState();
 
-  const stats = products?.map((item) => <PhoneCard product={item} key={item._id} />);
+  const handleSearchButton = () => {
+    // console.log(searchValue);
+    const filterPhone = products
+      ?.map((product) => {
+        if (product.brand === searchValue) {
+          return product;
+          // <PhoneCard product={product} key={product._id} />;
+        }
+      })
+      .filter(Boolean);
+
+    // const searchTotalItems = ());
+    setAllProducts(filterPhone?.map((item) => <PhoneCard product={item} key={item._id} />));
+  };
+
+  // console.log(allProducts?.length);
+
+  useEffect(() => {
+    const stats = products?.map((item) =>
+      item ? <PhoneCard product={item} key={item._id} /> : <p key={item._id}>There have no matching products</p>,
+    );
+    // console.log("products", stats);
+    setAllProducts(stats);
+  }, [products]);
+
+  //
   return (
-    products?.length > 0 && (
-      <Box id="ad" className="p-4 xs:p-10  bg-no-repeat bg-cover rounded-xl bg-featured shadow-md ">
-        <Container size="xl" className="bg-main-5/50 p-6 rounded-md">
-          <Stack className="mb-5">
-            <Title color="white" className="" order={2}>
-              Featured Phones
-            </Title>
-          </Stack>
-          <SimpleGrid className="sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xxl:grid-cols-4">{stats}</SimpleGrid>
-        </Container>
-      </Box>
-    )
+    <>
+      {allProducts?.length > 0 && (
+        <Box id="ad" className="p-4 xs:p-10  bg-no-repeat bg-cover rounded-xl bg-featured shadow-md ">
+          {/* <p style={{ marginBottom: "0px" }}>Search</p> */}
+          <Container>
+            <TextInput
+              icon={<IconSearch size="1.1rem" stroke={1.5} />}
+              radius="xl"
+              size="md"
+              className="mb-5 mt-0 p-0"
+              style={{ width: "100%", height: "" }}
+              rightSection={
+                <ActionIcon
+                  onClick={handleSearchButton}
+                  size={32}
+                  radius="xl"
+                  color={theme.primaryColor}
+                  variant="filled"
+                >
+                  {theme.dir === "ltr" ? (
+                    <IconArrowRight size="1.1rem" stroke={1.5} />
+                  ) : (
+                    <IconArrowLeft size="1.1rem" stroke={1.5} />
+                  )}
+                </ActionIcon>
+              }
+              placeholder="Search your phone"
+              rightSectionWidth={42}
+              onChange={(e) => {
+                setSearchValue(e.target.value);
+              }}
+            />
+          </Container>
+
+          <Container size="xl" className="bg-main-5/50 p-6 rounded-md">
+            <Stack className="mb-5">
+              <Title color="white" className="" order={2}>
+                Featured Phones
+              </Title>
+            </Stack>
+            <SimpleGrid className="sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xxl:grid-cols-4">
+              {allProducts}
+            </SimpleGrid>
+          </Container>
+        </Box>
+      )}
+
+      {allProducts?.length === 0 && (
+        <Box id="ad" className="p-4 xs:p-10  bg-no-repeat bg-cover rounded-xl bg-featured shadow-md ">
+          {/* <p style={{ marginBottom: "0px" }}>Search</p> */}
+          <Container>
+            <TextInput
+              icon={<IconSearch size="1.1rem" stroke={1.5} />}
+              radius="xl"
+              size="md"
+              className="mb-5 mt-0 p-0"
+              style={{ width: "100%", height: "" }}
+              rightSection={
+                <ActionIcon
+                  onClick={handleSearchButton}
+                  size={32}
+                  radius="xl"
+                  color={theme.primaryColor}
+                  variant="filled"
+                >
+                  {theme.dir === "ltr" ? (
+                    <IconArrowRight size="1.1rem" stroke={1.5} />
+                  ) : (
+                    <IconArrowLeft size="1.1rem" stroke={1.5} />
+                  )}
+                </ActionIcon>
+              }
+              placeholder="Search your phone"
+              rightSectionWidth={42}
+              onChange={(e) => {
+                setSearchValue(e.target.value);
+              }}
+            />
+          </Container>
+
+          <Container size="xl" className="bg-main-5/50 p-6 rounded-md">
+            <Stack className="mb-5">
+              <Title color="white" className="" order={2}>
+                Featured Phones
+              </Title>
+            </Stack>
+            <p>There have no matching phones.</p>
+          </Container>
+        </Box>
+      )}
+    </>
   );
 }
--- src/pages/products/mobile-phones/phone-card/PhoneCard.jsx
+++ src/pages/products/mobile-phones/phone-card/PhoneCard.jsx
@@ -12,18 +12,26 @@ export default function PhoneCard({ product }) {
   const { user: seller, userLoading: sellerLoading } = useGetAUser(createdBy);
 
   return (
-    <Card radius="md" withBorder p="xl" className="flex-col flex justify-between">
-      <Card.Section>
-        <PhonePhotos imageLinks={imageLinks} />
-        <CreatedTime createdAt={createdAt} />
-        <Details {...product} seller={seller} />
-      </Card.Section>
-      <Card.Section className="px-4 pb-4 mt-3">
-        <Group position="apart">
-          <Price price={price} />
-          <ActionButtons product={product} sellerLoading={sellerLoading} />
-        </Group>
-      </Card.Section>
-    </Card>
+    <>
+      {product ? (
+        <div>
+          <Card radius="md" withBorder p="xl" className="flex-col flex justify-between">
+            <Card.Section>
+              <PhonePhotos imageLinks={imageLinks} />
+              <CreatedTime createdAt={createdAt} />
+              <Details {...product} seller={seller} />
+            </Card.Section>
+            <Card.Section className="px-4 pb-4 mt-3">
+              <Group position="apart">
+                <Price price={price} />
+                <ActionButtons product={product} sellerLoading={sellerLoading} />
+              </Group>
+            </Card.Section>
+          </Card>
+        </div>
+      ) : (
+        <p>There have no matching products</p>
+      )}
+    </>
   );
 }
